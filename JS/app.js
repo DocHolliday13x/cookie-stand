b0e05929bcf801437cd7d16827930991a87628e9
Ryan Eastman <rking1323@gmail.com>
2023-02-13 21:59:29 +0000

    basic setup of files for lab06

---
+++ JS/app.js
@@ -0,0 +1,54 @@
+// TODO: figure out what info we need to display
+// * Store Location
+// * Hours of Operation
+// * Minimum Number of Customers per Hour
+// * Maximum Number of Customers per Hour
+// * Average Number of Cookies Sold
+
+// ********** GLOBALS **********
+let hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+// ********** HELPER FUNCTIONS/UTILITIES **********
+
+// ********** OBJECT LITERALS **********
+let seattle = {
+  name: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookiesPurchased: 6.3,
+  cookiesPurchased: []
+};
+
+let tokyo = {
+  name: 'Tokyo',
+  minCust: 3,
+  maxCust: 24,
+  avgCookiesPurchased: 1.2,
+  cookiesPurchased: []
+};
+
+let dubai = {
+  name: 'Dubai',
+  minCust: 11,
+  maxCust: 38,
+  avgCookiesPurchased: 3.7,
+  cookiesPurchased: []
+};
+
+let paris = {
+  name: 'Paris',
+  minCust: 20,
+  maxCust: 38,
+  avgCookiesPurchased: 2.3,
+  cookiesPurchased: []
+};
+
+let lima = {
+  name: 'Lima',
+  minCust: 2,
+  maxCust: 16,
+  avgCookiesPurchased: 4.6,
+  cookiesPurchased: []
+};
+
+// ********** EXECUTABLE CODE **********
